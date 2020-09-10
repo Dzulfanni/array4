e7a8f9efc9a2f646bbe4b561bb7e1776eb938d75
Dzulfanni <belajarayok866@gmail.com>
2020-09-11 06:50:49 +0700

    first commit

---
+++ array4.js
@@ -0,0 +1,6 @@
+var nilaiSantri = [ 5, 9, 6, 7, 9, 8, 10, 7, 8 ];
+var rataRata = 0;
+for(i = 0; i<nilaiSantri.length; i++){
+    rataRata += nilaiSantri[i]
+}
+document.write(rataRata/nilaiSantri.length);
